36c56fd688f06763a6e6abe817f3a725c005c7e1
Danny Gilbert <danny_gilbert@icloud.com>
2017-10-24 14:13:27 -0500

    more examples of immutability

--- Immutability.js
+++ Immutability.js
@@ -4,3 +4,34 @@ let color_lawn = {
     rating: 0
 }
 
+var modifyOriginal_rateColor = (color, rating) => {
+    color.rating = rating
+    return color
+}
+
+console.log(modifyOriginal_rateColor(color_lawn, 5).rating)
+console.log(color_lawn.rating)
+
+var modifyCopy_rateColor = (color, rating) => {
+    return Object.assign({}, color, {rating: rating})
+}
+
+console.log(modifyCopy_rateColor(color_lawn, 9).rating)
+console.log(color_lawn.rating)
+
+let list = [
+    { title: 'Rad Red' },
+    { title: 'Lawn' },
+    { title: 'Party Pink' }
+]
+
+var push_addColor = (title, colors) => {
+    colors.push({ title: title })
+    return colors
+}
+console.log(push_addColor('Glam Green', list))
+
+const concat_addColor = (title, array) => [...list, {title}]
+console.log(concat_addColor('Better Blue', list))
+console.log(list)
+
