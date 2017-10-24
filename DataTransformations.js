d016261948eb160eebc1c095661fe81e174876d6
Danny Gilbert <danny_gilbert@icloud.com>
2017-10-24 15:59:59 -0500

    Updated Data Transformations page

--- DataTransformations.js
+++ DataTransformations.js
@@ -33,12 +33,12 @@ console.log(schoolArray)
 
 const ages = [21, 18, 42, 40, 64, 63, 34]
 
-const maxAge = ages.reduce((max, age) => {
-    console.log(`${age} > ${max} = ${age > max}`)
-    if (age > max) {
-        return age
-    } else {
-        return max
-    }
-}, 0)
+const maxAge = ages.reduce(
+    (max, value) => (value > max) ? value : max, ages[0]
+)
 console.log('maxAge', maxAge)
+
+const minAge = ages.reduceRight(
+    (min, value) => (value < min) ? value : min, ages[ages.length-1]
+)
+console.log(minAge)
